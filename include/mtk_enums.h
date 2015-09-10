4a9481089803fd9bc59b6b7e1495a1b2b83a15fd
ejspeiro <ejspeiro@gmail.com>
2015-09-10 01:18:35 -0700

    Create mtk_enums.h file to define enumerations used in the entire library.

---
+++ include/mtk_enums.h
@@ -0,0 +1,118 @@
+/*!
+\file mtk_enums.h
+
+\brief Considered enumeration types in the MTK.
+
+Enumeration types are used throughout the MTK to differentiate instances of
+derived classes, as well as for mnemonic purposes. In this file, the enumeration
+types are listed alphabetically.
+
+\author: Eduardo J. Sanchez (ejspeiro) - esanchez at mail dot sdsu dot edu
+*/
+/*
+Copyright (C) 2015, Computational Science Research Center, San Diego State
+University. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Modifications to source code should be reported to: esanchez@mail.sdsu.edu
+and a copy of the modified files should be reported once modifications are
+completed. Documentation related to said modifications should be included.
+
+2. Redistributions of source code must be done through direct
+downloads from the project's GitHub page: http://www.csrc.sdsu.edu/mtk
+
+3. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+4. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+5. Usage of the binary form on proprietary applications shall require explicit
+prior written permission from the the copyright holders.
+
+6. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+The copyright holders provide no reassurances that the source code provided does
+not infringe any patent, copyright, or any other intellectual property rights of
+third parties. The copyright holders disclaim any liability to any recipient for
+claims brought against recipient by any third party for infringement of that
+parties intellectual property rights.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef MTK_INCLUDE_ENUMS_H_
+#define MTK_INCLUDE_ENUMS_H_
+
+namespace mtk {
+
+/*!
+\enum MatrixStorage
+
+\ingroup c02-enums
+
+\brief Considered matrix storage schemes to implement sparse matrices.
+
+The considered sparse storage schemes are selected so that these are compatible
+with some of the most used mathematical APIs, as follows: DENSE and BANDED for
+<a href="http://www.netlib.org/blas/">BLAS</a>,
+<a href="http://www.netlib.org/lapack/">LAPACK</a>, and
+<a href="http://www.netlib.org/scalapack/">ScaLAPACK</a>. Finally, CRS for
+<a href="http://crd.lbl.gov/~xiaoye/SuperLU/">SuperLU</a>.
+*/
+enum MatrixStorage {
+  DENSE,    ///< Dense matrices, implemented as a 1D array: DenseMatrix.
+  BANDED,   ///< Banded matrices ala LAPACK and ScaLAPACK: Must be implemented.
+  CRS       ///< Compressed-Rows Storage: Must be implemented.
+};
+
+/*!
+\enum MatrixOrdering
+
+\ingroup c02-enums
+
+\brief Considered matrix ordering (for Fortran purposes).
+
+Row-major ordering is used for most application in C/C++. For Fortran purposes,
+the matrices must be listed in a column-major ordering.
+
+\sa https://en.wikipedia.org/wiki/Row-major_order
+*/
+enum MatrixOrdering {
+  ROW_MAJOR,  ///< Row-major ordering (C/C++).
+  COL_MAJOR   ///< Column-major ordering (Fortran).
+};
+
+/*!
+\enum FieldNature
+
+\ingroup c02-enums
+
+\brief Nature of the field discretized in a given grid.
+
+Fields can be **scalar** or **vector** in nature.
+
+\sa https://en.wikipedia.org/wiki/Scalar_field
+
+\sa https://en.wikipedia.org/wiki/Vector_field
+*/
+enum FieldNature {
+  SCALAR,  ///< Scalar-valued field.
+  VECTOR   ///< Vector-valued field.
+};
+}
+#endif  // End of: MTK_INCLUDE_ENUMS_H_
